5cb9ea2c3cc44511eb2385188d7d5e00080ee41b
Ahmet Bozaci <ahmt9417@gmail.com>
2022-07-11 12:31:50 +0300

    Add app css file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import Details from './components/Details';
 import Search from './components/Search';
+import './App.css';
 
 const App = () => (
   <BrowserRouter>
